e9423496319be52034fa9636f183fca8e6bb4b4e
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-11-26 17:33:24 -0300

    AMF-01 Create mvc portlet module

---
+++ modules/registration/registration-web/src/main/java/com/acme/registration/constants/RegistrationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.acme.registration.constants;
+
+/**
+ * @author me
+ */
+public class RegistrationPortletKeys {
+
+	public static final String REGISTRATION =
+		"com_acme_registration_RegistrationPortlet";
+
+}
