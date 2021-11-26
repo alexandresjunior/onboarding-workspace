1fd23a60b1b4ed0b5b352237eb6dd88e9a4c8b15
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-11-26 16:59:33 -0300

    AMF-01 Define account entity; build service

---
+++ modules/registration/registration-service/src/main/java/com/acme/registration/model/impl/AccountImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.acme.registration.model.impl;
+
+/**
+ * The extended model implementation for the Account service. Represents a row in the &quot;AMF_Account&quot; database table, with each column mapped to a property of this class.
+ *
+ * <p>
+ * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.acme.registration.model.Account</code> interface.
+ * </p>
+ *
+ * @author Alexandre de Souza Jr.
+ */
+public class AccountImpl extends AccountBaseImpl {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. All methods that expect a account model instance should use the {@link com.acme.registration.model.Account} interface instead.
+	 */
+	public AccountImpl() {
+	}
+
+}
---
+++ modules/registration/registration-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,2 @@
+create index IX_BF2E53A on AMF_Account (companyId);
+create index IX_24089A6 on AMF_Account (groupId, userName[$COLUMN_LENGTH:75$]);
