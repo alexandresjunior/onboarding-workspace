e4de3489a41710b5045c49e6765642da2d345056
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-11-29 09:06:38 -0300

    AMF-01 Add portlet keys

--- modules/registration/registration-web/src/main/java/com/acme/registration/constants/RegistrationPortletKeys.java
+++ modules/registration/registration-web/src/main/java/com/acme/registration/constants/RegistrationPortletKeys.java
@@ -1,11 +1,14 @@
 package com.acme.registration.constants;
 
 /**
- * @author me
+ * @author Alexandre de Souza Jr.
  */
 public class RegistrationPortletKeys {
 
 	public static final String REGISTRATION =
 		"com_acme_registration_RegistrationPortlet";
+
+	public static final String REGISTRATION_MVC_COMMAND_NAME =
+		"/registration-web/register_account";
 
 }
