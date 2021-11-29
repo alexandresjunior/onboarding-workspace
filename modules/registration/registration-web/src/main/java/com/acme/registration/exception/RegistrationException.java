bc760db6c13dab2c41b3a2906a6bc16dc05bbc8d
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-11-29 09:07:22 -0300

    AMF-01 Create registration exception

---
+++ modules/registration/registration-web/src/main/java/com/acme/registration/exception/RegistrationException.java
@@ -0,0 +1,12 @@
+package com.acme.registration.exception;
+
+/**
+ * @author Alexandre de Souza Jr.
+ */
+public class RegistrationException extends Exception {
+
+	public RegistrationException(String message) {
+		super(message);
+	}
+
+}
