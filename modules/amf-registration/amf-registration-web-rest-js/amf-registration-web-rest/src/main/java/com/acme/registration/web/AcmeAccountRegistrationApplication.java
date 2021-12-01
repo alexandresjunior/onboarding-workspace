e51fd71e949ad0d14230aede4ff7da0187ee745b
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-12-01 14:44:28 -0300

    AMF-01 Add endpoint add-user into web rest module

--- modules/amf-registration/amf-registration-web-rest-js/amf-registration-web-rest/src/main/java/com/acme/registration/web/AcmeAccountRegistrationApplication.java
+++ modules/amf-registration/amf-registration-web-rest-js/amf-registration-web-rest/src/main/java/com/acme/registration/web/AcmeAccountRegistrationApplication.java
@@ -2,13 +2,14 @@ package com.acme.registration.web;
 
 import com.acme.registration.model.Account;
 import com.acme.registration.service.AccountLocalService;
+
 import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+import java.util.Collections;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,38 +18,46 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
 /**
  * @author Alexandre de Souza Jr.
  */
 @Component(
-    property = {
-            JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/account-registration",
-            JaxrsWhiteboardConstants.JAX_RS_NAME + "=AcmeAccountRegistration.Rest"
-    },
-    service = Application.class
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/account-registration",
+		JaxrsWhiteboardConstants.JAX_RS_NAME + "=AcmeAccountRegistration.Rest"
+	},
+	service = Application.class
 )
-public class AcmeRegistrationApplication extends Application {
+public class AcmeAccountRegistrationApplication extends Application {
 
-    @Path("/add-account")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addAccount(String body) {
-        Account account = _jSONFactory.looseDeserialize(body, Account.class);
+	@Path("/add-account")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addAccount(String body) {
+		Account account = _jSONFactory.looseDeserialize(body, Account.class);
 
-        account = _accountLocalService.addAccount(account);
+		account = _accountLocalService.addAccount(account);
 
-        return Response.ok(
-            _jSONFactory.looseSerialize(account)
-        ).build();
-    }
+		return Response.ok(
+			_jSONFactory.looseSerialize(account)
+		).build();
+	}
 
-    @Context
-    private HttpServletRequest _request;
+	public Set<Object> getSingletons() {
+		return Collections.singleton(this);
+	}
 
-    @Reference
-    private JSONFactory _jSONFactory;
+	@Reference
+	private AccountLocalService _accountLocalService;
 
-    @Reference
-    private AccountLocalService _accountLocalService;
+	@Reference
+	private JSONFactory _jSONFactory;
+
+	@Context
+	private HttpServletRequest _request;
 
 }
