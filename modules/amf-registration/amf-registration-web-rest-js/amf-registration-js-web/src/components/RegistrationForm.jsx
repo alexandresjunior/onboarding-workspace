6990f389a3e8784549e1ff339cf592c022e4054a
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-12-07 15:15:26 -0300

    AMF-01 Create form components

---
+++ modules/amf-registration/amf-registration-web-rest-js/amf-registration-js-web/src/components/RegistrationForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import BasicInfo from "./BasicInfo";
+import PhoneInfo from "./PhoneInfo";
+import BillingAddressInfo from "./BillingAddressInfo";
+import MiscInfo from "./MiscInfo";
+import TermsOfUse from "./TermsOfUse";
+
+function RegistrationForm() {
+    return (
+        <>
+            <form action="#">
+                <BasicInfo />
+                <PhoneInfo />
+                <BillingAddressInfo />
+                <MiscInfo />
+                <TermsOfUse />
+
+                <div className="sheet-footer sheet-footer-btn-block-sm-down">
+                    <div className="btn-group">
+                        <div className="btn-group-item">
+                            <button className="btn btn-primary" type="button">Register</button>
+                        </div>
+                        <div className="btn-group-item">
+                            <button className="btn btn-secondary" type="button">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+};
+
+export default RegistrationForm;
