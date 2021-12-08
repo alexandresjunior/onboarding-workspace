54492f081a7e715413c0819a9501d9a01108f438
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-12-08 11:40:22 -0300

    AMF-01 Update bundles' names; add react components

--- modules/amf-registration/amf-registration-web-rest-js/amf-registration-web-js/src/AppComponent.js
+++ modules/amf-registration/amf-registration-web-rest-js/amf-registration-web-js/src/AppComponent.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RegistrationForm from "./components/RegistrationForm";
 
 export default class extends React.Component {
 	render() {
 		return (
-            <div>
-				<div>
-        	        <span className="tag">{Liferay.Language.get('portlet-namespace')}:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
+			<div className="container">
+				<div className='text-center'>
+					<h1 className='font-weight-normal mb-3'>
+						{Liferay.Language.get('amf.registration.caption')}
+					</h1>
 				</div>
-				<div>
-    	            <span className="tag">{Liferay.Language.get('context-path')}:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">{Liferay.Language.get('portlet-element-id')}:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
-				
+				<RegistrationForm />
 			</div>
 		);
 	}	
