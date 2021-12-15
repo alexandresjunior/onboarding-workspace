0a636e2a063776f44a8612f5ca0807c9b7c1135c
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-12-15 12:41:08 -0300

    AMF-02 Create event monitor web jsp module

---
+++ modules/amf-event-monitor/amf-event-monitor-web-jsp/amf-event-monitor-web/src/main/java/amf/event/monitor/web/constants/EventMonitorPortletKeys.java
@@ -0,0 +1,11 @@
+package amf.event.monitor.web.constants;
+
+/**
+ * @author me
+ */
+public class EventMonitorPortletKeys {
+
+	public static final String EVENTMONITOR =
+		"amf_event_monitor_web_EventMonitorPortlet";
+
+}
