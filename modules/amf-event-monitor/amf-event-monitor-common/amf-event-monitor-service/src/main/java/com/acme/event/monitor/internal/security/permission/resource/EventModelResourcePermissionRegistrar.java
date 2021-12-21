66bc6e4a45d2f28d6643f0032cd45068b398e519
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-12-21 15:49:06 -0300

    AMF-02 Registering permissions with the container

---
+++ modules/amf-event-monitor/amf-event-monitor-common/amf-event-monitor-service/src/main/java/com/acme/event/monitor/internal/security/permission/resource/EventModelResourcePermissionRegistrar.java
@@ -0,0 +1,77 @@
+package com.acme.event.monitor.internal.security.permission.resource;
+
+import com.acme.event.monitor.constants.EventMonitorConstants;
+import com.acme.event.monitor.constants.EventMonitorPortletKeys;
+import com.acme.event.monitor.model.Event;
+import com.acme.event.monitor.service.EventLocalService;
+import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.StagedModelPermissionLogic;
+import com.liferay.portal.kernel.security.permission.resource.WorkflowedModelPermissionLogic;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.workflow.permission.WorkflowPermission;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Dictionary;
+
+/**
+ * @author Alexandre de Souza Jr.
+ */
+@Component(immediate = true)
+public class EventModelResourcePermissionRegistrar {
+
+    @Activate
+    public void activate(BundleContext bundleContext) {
+        Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+        properties.put("model.class.name", Event.class.getName());
+
+        _serviceRegistration = bundleContext.registerService(
+                ModelResourcePermission.class,
+                ModelResourcePermissionFactory.create(
+                        Event.class, Event::getEventId,
+                        _eventLocalService::getEvent, _portletResourcePermission,
+                        (modelResourcePermission, consumer) -> {
+                            consumer.accept(
+                                    new StagedModelPermissionLogic<>(
+                                            _stagingPermission, EventMonitorPortletKeys.EVENT_MONITOR,
+                                            Event::getEventId));
+                            consumer.accept(
+                                    new WorkflowedModelPermissionLogic<>(
+                                            _workflowPermission, modelResourcePermission,
+                                            _groupLocalService, Event::getEventId));
+                        }),
+                properties);
+    }
+
+    @Deactivate
+    public void deactivate() {
+        _serviceRegistration.unregister();
+    }
+
+    @Reference
+    private EventLocalService _eventLocalService;
+
+    @Reference(target = "(resource.name=" + EventMonitorConstants.RESOURCE_NAME + ")")
+    private PortletResourcePermission _portletResourcePermission;
+
+    private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+    @Reference
+    private StagingPermission _stagingPermission;
+
+    @Reference
+    private WorkflowPermission _workflowPermission;
+
+    @Reference
+    private GroupLocalService _groupLocalService;
+
+}
