fd614e247dbc911af3594c3e7799794f2a19be6c
Ricardo Moura <ricardo.moura@liferay.com>
2022-01-04 14:46:35 -0300

    BE-03 Create AccountDTO for that JSON parsing be possible

---
+++ modules/amf-registration/amf-registration-web-rest-js/amf-registration-web-rest/src/main/java/com/acme/registration/dto/AccountDTO.java
@@ -0,0 +1,270 @@
+package com.acme.registration.dto;
+
+import com.acme.registration.model.Account;
+
+public class AccountDTO {
+    private long accountId;
+    private long groupId;
+    private long userId;
+    private long companyId;
+    private String createDate;
+    private String modifiedDate;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String userName;
+    private String genre;
+    private String birthday;
+    private String password;
+    private String confirmPassword;
+    private String homePhone;
+    private String mobilePhone;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String securityQuestion;
+    private String answer;
+    private boolean termsOfUse;
+
+    private AccountDTO(long accountId, long groupId,
+                      long userId, long companyId, String createDate,
+                      String modifiedDate, String firstName,
+                      String lastName, String emailAddress,
+                      String userName, String genre, String birthday,
+                      String password, String confirmPassword,
+                      String homePhone, String mobilePhone,
+                      String address1, String address2, String city,
+                      String state, String zipCode, String securityQuestion,
+                      String answer, boolean termsOfUse) {
+
+        this.accountId = accountId;
+        this.groupId = groupId;
+        this.userId = userId;
+        this.companyId = companyId;
+        this.createDate = createDate;
+        this.modifiedDate = modifiedDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.userName = userName;
+        this.genre = genre;
+        this.birthday = birthday;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.homePhone = homePhone;
+        this.mobilePhone = mobilePhone;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.securityQuestion = securityQuestion;
+        this.answer = answer;
+        this.termsOfUse = termsOfUse;
+    }
+
+    public static AccountDTO fromAccount(Account account){
+        return new AccountDTO(account.getAccountId(), account.getGroupId(),
+                account.getUserId(), account.getCompanyId(), account.getCreateDate().toString(),
+                account.getModifiedDate().toString(), account.getFirstName(), account.getLastName(),
+                account.getEmailAddress(), account.getUserName(), account.getGenre(),
+                account.getBirthday().toString(), account.getPassword(), account.getConfirmPassword(),
+                account.getHomePhone(), account.getMobilePhone(), account.getAddress1(),
+                account.getAddress2(), account.getCity(),account.getState(),account.getZipCode(),
+                account.getSecurityQuestion(), account.getAnswer(),account.getTermsOfUse());
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isTermsOfUse() {
+        return termsOfUse;
+    }
+
+    public void setTermsOfUse(boolean termsOfUse) {
+        this.termsOfUse = termsOfUse;
+    }
+}
